6b529e73b304ac222e59d924d54592e2e7c8a916
Juan Lee <me@juany.kr>
2025-10-30 11:18:52 +0900

    feat: Refactor Terraform configuration and add outputs
    - Refactored main.tf for improved readability by aligning variable assignments.
    - Added outputs for VPC, subnets, S3 bucket, RDS endpoint, and DynamoDB table details to enhance visibility and management of resources.
    - Included ECS service name and ALB DNS name in outputs for better integration with other modules.

--- env/dev/main.tf
+++ env/dev/main.tf
@@ -22,35 +22,41 @@ module "vpc" {
   prefix       = var.prefix
   project_name = var.project_name
   environment  = var.environment
-  region       = var.region
-  vpc_cidr     = var.vpc_cidr
+
+  region   = var.region
+  vpc_cidr = var.vpc_cidr
 }
 
 module "bastion" {
   source = "../../modules/ec2"
 
-  prefix                = var.prefix
-  project_name          = var.project_name
-  environment           = var.environment
-  region                = var.region
-  vpc_id                = module.vpc.vpc_id
-  subnet_id             = module.vpc.public_subnet_ids[0]
+  prefix       = var.prefix
+  project_name = var.project_name
+  environment  = var.environment
+
+  region    = var.region
+  vpc_id    = module.vpc.vpc_id
+  subnet_id = module.vpc.public_subnet_ids[0]
+
+  instance_name         = "bastion"
   instance_type         = "t3.small"
   key_name              = "ec2-kp"
   instance_profile_name = module.iam.poweruser_instance_profile_name
-  instance_name         = "bastion"
 }
 
 module "s3" {
   source = "../../modules/s3"
 
-  prefix               = var.prefix
-  project_name         = var.project_name
+  prefix       = var.prefix
+  project_name = var.project_name
+
   enable_public_access = true
   enable_versioning    = true
   enable_hosting       = true
-  index_document       = "index.html"
-  error_document       = "error.html"
+
+  index_document = "index.html"
+  error_document = "error.html"
+
   cors_allowed_headers = ["*"]
   cors_allowed_methods = ["GET"]
   cors_allowed_origins = ["*"]
@@ -80,9 +86,8 @@ module "rds" {
   allow_ingress_from_vpc     = false
   allowed_security_group_ids = [module.bastion.security_group_id]
 
-  backup_window      = "03:00-04:00"
-  maintenance_window = "sun:05:00-sun:06:00"
-
+  backup_window          = "03:00-04:00"
+  maintenance_window     = "sun:05:00-sun:06:00"
   parameter_group_family = "mysql8.0"
 
   cloudwatch_logs_exports      = ["error", "slowquery"]
@@ -97,6 +102,7 @@ module "dynamodb" {
   prefix       = var.prefix
   project_name = var.project_name
   environment  = var.environment
+
   table_name   = "ddbt"
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "id"
@@ -111,14 +117,17 @@ module "dynamodb" {
 module "ecs" {
   source = "../../modules/ecs"
 
-  region                  = var.region
-  vpc_id                  = module.vpc.vpc_id
-  service_subnet_ids      = module.vpc.private_subnet_ids
-  alb_subnet_ids          = module.vpc.public_subnet_ids
-  account_id              = data.aws_caller_identity.current.account_id
-  prefix                  = var.prefix
-  project_name            = var.project_name
-  environment             = var.environment
+  prefix       = var.prefix
+  project_name = var.project_name
+  environment  = var.environment
+
+  region     = var.region
+  vpc_id     = module.vpc.vpc_id
+  account_id = data.aws_caller_identity.current.account_id
+
+  service_subnet_ids = module.vpc.private_subnet_ids
+  alb_subnet_ids     = module.vpc.public_subnet_ids
+
   repository_name         = "sigmoid-app"
   image_tag_mutability    = "MUTABLE"
   keep_last_n_images      = 10
